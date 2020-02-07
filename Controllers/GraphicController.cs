1e6a98b3fee441ec734bcd8753b7b862f7cb63f7
pjruddy1 <ruddypj.pr@gmail.com>
2020-02-07 08:43:32 -0500

    trouble with initializer

---
+++ Controllers/GraphicController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SouthgateMobileVillage.DAL;
+using SouthgateMobileVillage.Models;
+
+namespace SouthgateMobileVillage.Controllers
+{
+    public class GraphicController : Controller
+    {
+        private ResidentContext db = new ResidentContext();
+
+        // GET: Graphic
+        public ActionResult Index()
+        {
+            var graphics = db.Graphics.Include(g => g.Home);
+            return View(graphics.ToList());
+        }
+
+        // GET: Graphic/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Graphic graphic = db.Graphics.Find(id);
+            if (graphic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(graphic);
+        }
+
+        // GET: Graphic/Create
+        public ActionResult Create()
+        {
+            ViewBag.HomeID = new SelectList(db.Homes, "ID", "ID");
+            return View();
+        }
+
+        // POST: Graphic/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,HomeID,Photo,AltText")] Graphic graphic)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Graphics.Add(graphic);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.HomeID = new SelectList(db.Homes, "ID", "ID", graphic.HomeID);
+            return View(graphic);
+        }
+
+        // GET: Graphic/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Graphic graphic = db.Graphics.Find(id);
+            if (graphic == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.HomeID = new SelectList(db.Homes, "ID", "ID", graphic.HomeID);
+            return View(graphic);
+        }
+
+        // POST: Graphic/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,HomeID,Photo,AltText")] Graphic graphic)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(graphic).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.HomeID = new SelectList(db.Homes, "ID", "ID", graphic.HomeID);
+            return View(graphic);
+        }
+
+        // GET: Graphic/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Graphic graphic = db.Graphics.Find(id);
+            if (graphic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(graphic);
+        }
+
+        // POST: Graphic/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Graphic graphic = db.Graphics.Find(id);
+            db.Graphics.Remove(graphic);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/ResidentController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SouthgateMobileVillage.DAL;
+using SouthgateMobileVillage.Models;
+
+namespace SouthgateMobileVillage.Controllers
+{
+    public class ResidentController : Controller
+    {
+        private ResidentContext db = new ResidentContext();
+
+        // GET: Resident
+        public ActionResult Index()
+        {
+            var residents = db.Residents.Include(r => r.Home);
+            return View(residents.ToList());
+        }
+
+        // GET: Resident/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resident resident = db.Residents.Find(id);
+            if (resident == null)
+            {
+                return HttpNotFound();
+            }
+            return View(resident);
+        }
+
+        // GET: Resident/Create
+        public ActionResult Create()
+        {
+            ViewBag.HomeID = new SelectList(db.Homes, "ID", "ID");
+            return View();
+        }
+
+        // POST: Resident/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,HomeID,FirstName,LastName")] Resident resident)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Residents.Add(resident);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.HomeID = new SelectList(db.Homes, "ID", "ID", resident.HomeID);
+            return View(resident);
+        }
+
+        // GET: Resident/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resident resident = db.Residents.Find(id);
+            if (resident == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.HomeID = new SelectList(db.Homes, "ID", "ID", resident.HomeID);
+            return View(resident);
+        }
+
+        // POST: Resident/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,HomeID,FirstName,LastName")] Resident resident)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(resident).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.HomeID = new SelectList(db.Homes, "ID", "ID", resident.HomeID);
+            return View(resident);
+        }
+
+        // GET: Resident/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resident resident = db.Residents.Find(id);
+            if (resident == null)
+            {
+                return HttpNotFound();
+            }
+            return View(resident);
+        }
+
+        // POST: Resident/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Resident resident = db.Residents.Find(id);
+            db.Residents.Remove(resident);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
