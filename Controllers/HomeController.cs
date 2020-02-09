c944d307f04dc0c200965cb05f33e5b7484b2be5
pjruddy1 <ruddypj.pr@gmail.com>
2020-02-09 08:10:56 -0500

    working on home controller index view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,30 @@
-﻿using System;
+﻿using SouthgateMobileVillage.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SouthgateMobileVillage.Models;
 
 namespace SouthgateMobileVillage.Controllers
 {
+    
     public class HomeController : Controller
     {
+
+        private ResidentContext db = new ResidentContext();
+
         public ActionResult Index()
         {
-            return View();
+            List<Home> homes = db.Homes.ToList();
+            List<Graphic> graphics = db.Graphics.ToList();
+            var query = from h in homes
+                        join gr in graphics on h.ID equals gr.HomeID into table1
+                        from gr in table1.DefaultIfEmpty()
+                        select new JoinTable { GetHome = h, GetGraphic = gr };
+                       
+
+            return View(query.ToList()) ;
         }
 
         public ActionResult About()
